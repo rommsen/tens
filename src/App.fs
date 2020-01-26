a912175b1a5475a39c3bf94ff485c958a3b4ee0c
Roman Sachse <roman.sachse@googlemail.com>
2020-01-26 22:48:55 +0100

    first version of update

--- src/App.fs
+++ src/App.fs
@@ -4,23 +4,63 @@ open Elmish
 open Elmish.React
 open Feliz
 
+type Running =
+  {
+    Clicked : int list
+    Numbers : int list
+    Points : int
+  }
+
 type State =
-    { Count: int }
+  | Not_Started
+  | Running of Running 
+  | Finished of int
 
 type Msg =
-    | Increment
-    | Decrement
+    | Tick of int
+    | Started
+    | Clicked of int 
 
 let init() =
-    { Count = 0 }
+  Not_Started
+
+let removeIndex index list =
+  list 
+  |> List.mapi (fun i element -> (i <> index, element))
+  |> List.filter fst 
+  |> List.map snd
 
 let update (msg: Msg) (state: State): State =
-    match msg with
-    | Increment ->
-        { state with Count = state.Count + 1 }
+    match state,msg with
+    | Running state, Clicked index ->
+        state.Numbers
+        |> List.tryItem index 
+        |> Option.map (fun number ->
+            let newState =
+              { state with 
+                  Clicked = state.Clicked @ [number] 
+                  Numbers = state.Numbers |> removeIndex index
+              }
 
-    | Decrement ->
-        { state with Count = state.Count - 1 }
+            if newState.Clicked |> List.sum = 10 then 
+              Running { state with Clicked = [] ; Points = state.Points + 1 }
+            elif newState.Clicked |> List.length = 3 then 
+              Finished state.Points
+            else
+              Running state
+          )
+        |> Option.defaultValue (Running state)    
+
+    | Running state, Tick number ->
+        if state.Numbers |> List.length = 11 then 
+          Finished state.Points
+        else 
+          Running { state with Numbers = state.Numbers @ [number] }
+
+    | _ -> state     
+        
+
+
 
 let render (state: State) (dispatch: Msg -> unit) =
   Html.div [
